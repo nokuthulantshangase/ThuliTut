eedf1670e34cec95bc81aa206f0ef62f43850c6e
Thuli <thuli@genpoint.co.za>
2017-01-19 09:28:02 +0200

    Updated

---
+++ Caliburn Micro/Caliburn Micro/AppBootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+
+namespace Caliburn_Micro
+{
+  public  class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+         DisplayRootViewFor<AppViewModel>();
+        }
+
+    }
+}
---
+++ Caliburn Micro/Caliburn Micro/AppViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn_Micro;
+using Caliburn.Micro;
+
+namespace Caliburn_Micro
+{
+  public  class AppViewModel: PropertyChangedBase
+    {
+        private int _message=10;
+        public int Message
+        {
+            get { return _message; }
+            set { _message = value;
+                NotifyOfPropertyChange(() => Message);
+                NotifyOfPropertyChange(()=>CanChangeMessage);
+            }
+        }
+        public bool CanChangeMessage
+        {
+            get { return _pressCount < 100; }
+        }
+        private int _pressCount;
+        public AppViewModel()
+        {
+            //Message = "Hello user";
+            _pressCount = 10;
+        }
+        public void ChangeMessage(int delta)
+        {
+            _pressCount+= delta;
+            Message =  _pressCount;
+        }
+    }
+}
